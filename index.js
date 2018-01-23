e43e30f36d161d17fbe487e0d37459981da2b1e1
Remka <remuka@gmail.com>
2018-01-23 20:04:37 +0900

    Small cosmetic changes

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
+'use strict'
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const http = require('http');
 const request = require('request');
+const app = express();
 
-const apiKey = '6309d892';
+const omdbApiKey = '6309d892';
 
-const server = express();
-server.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.set('port', (process.env.PORT || 5000));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
-server.use(bodyParser.json());
-
-server.post('/get-movie-details', function (req, res) {
+app.post('/get-movie-details', function (req, res) {
 
     let movieToSearch = req.body.result && req.body.result.parameters && req.body.result.parameters.movie ? req.body.result.parameters.movie : 'Blade Runner';
-    let reqUrl = encodeURI('https://www.omdbapi.com/?apikey='+ apiKey +'&t=' + movieToSearch);
-
+    let reqUrl = encodeURI('https://www.omdbapi.com/?apikey='+ omdbApiKey +'&t=' + movieToSearch);
 
     request(reqUrl, function(error, response, body) {
 
@@ -38,6 +37,7 @@ server.post('/get-movie-details', function (req, res) {
     } else {
 
       let dataToSend = 'We could not find any entry in the database, sorry.';
+
       res.json({
         speech: dataToSend,
         displayText: dataToSend,
@@ -45,11 +45,11 @@ server.post('/get-movie-details', function (req, res) {
       });
 
     }
-      //res.send(movie);
-    });
+
+  });
 
 });
 
-server.listen((process.env.PORT || 8000), function () {
-    console.log("Server is up and running...");
-});
+app.listen(app.get('port'), function() {
+	console.log('Running on port', app.get('port'));
+})
